d76c10abf8210dca147b59a1168c10639d45d76d
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-12-29 13:14:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 'use strict';
 
-const firstForm = document.querySelector('#first_form'),
-      meaningA  = firstForm.querySelector('#meaning_a'),
-      meaningB  = firstForm.querySelector('#meaning_b'),
-      meaningC  = firstForm.querySelector('#meaning_c'),
-      result    = document.querySelector('.result');
+// Изолирую код в анонимную само-вызывающую функцию
+(function () {
+    const firstForm = document.querySelector('#first_form'),
+        meaningA = firstForm.querySelector('#meaning_a'),
+        meaningB = firstForm.querySelector('#meaning_b'),
+        meaningC = firstForm.querySelector('#meaning_c'),
+        result = document.querySelector('.result');
 
-firstForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Отменить действия формы по умолчанию
+    firstForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // Отменить действия формы по умолчанию
 
-    // Принимаем данные из формы и преобразуем их в число с помощью Number()
-    const allValue = Number(meaningA.value) + Number(meaningB.value) + Number(meaningC.value);
+        // Принимаем данные из формы и преобразуем их в число с помощью Number()
+        const allValue = Number(meaningA.value) + Number(meaningB.value) + Number(meaningC.value);
 
-    // Метод Math.sqrt() возвращает квадратный корень числа
-    // Метод toFixed() количество цифр после десятичной запятой
-    // Функция isNaN() определяет является ли литерал или переменная нечисловым значением
-    if(!isNaN(allValue)) {
-          result.innerHTML = `Результат: ${Math.sqrt(allValue).toFixed(2)}`;
-          // Задаем синюю окраску текста
-          result.style.color = 'blue';
-    } else {
-          result.innerHTML = 'Введите в поле формы число!';
-          // Задаем красную окраску текста
-          result.style.color = 'red';
-    }
+        // Метод Math.sqrt() возвращает квадратный корень числа
+        // Метод toFixed() количество цифр после десятичной запятой
+        // Функция isNaN() определяет является ли литерал или переменная нечисловым значением
+        if (!isNaN(allValue)) {
+            result.innerHTML = `Результат: ${Math.sqrt(allValue).toFixed(2)}`;
+            // Задаем синюю окраску текста
+            result.style.color = 'blue';
+        } else {
+            result.innerHTML = 'Введите в поле формы число!';
+            // Задаем красную окраску текста
+            result.style.color = 'red';
+        }
 
-    firstForm.reset(); // Очищаем поля формы после ее отправки
-});
+        firstForm.reset(); // Очищаем поля формы после ее отправки
+    });
+}());
